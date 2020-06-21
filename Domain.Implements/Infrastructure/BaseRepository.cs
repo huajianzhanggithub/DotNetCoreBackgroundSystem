1124e375d27030a8d03a3b2d4c14301f234b7966
huajian zhang <278554189@qq.com>
2020-06-21 18:30:06 +0800

    2020年6月21日 18点30分

--- Domain.Implements/Infrastructure/BaseRepository.cs
+++ Domain.Implements/Infrastructure/BaseRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using Data.Infrastructure;
 using Domain.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Z.EntityFramework.Plus;
 
 namespace Domain.Implements.Infrastructure
 {
@@ -44,97 +46,109 @@ namespace Domain.Implements.Infrastructure
 
         public void Update(Expression<Func<T, bool>> predicate, Expression<Func<T, T>> updator)
         {
-            throw new NotImplementedException();
+            Set.Where(predicate).UpdateFromQuery(updator);
         }
 
         public void Delete(T entity)
         {
-            throw new NotImplementedException();
+            Set.Remove(entity);
         }
 
         public void Delete(params T[] entities)
         {
-            throw new NotImplementedException();
+            Set.RemoveRange(entities);
         }
 
         public void Delete(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            Set.Where(predicate).DeleteFromQuery();
         }
 
         public void DeleteByKey(object key)
         {
-            throw new NotImplementedException();
+            Delete(Set.Find(key));
         }
 
         public void DeleteByKeys(params object[] keys)
         {
-            throw new NotImplementedException();
+            foreach (var key in keys)
+            {
+                DeleteByKey(key);
+            }
         }
 
         public T Get(object key)
         {
-            throw new NotImplementedException();
+            return Set.Find(key);
         }
 
         public T Get(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Set.FirstOrDefault(predicate); // SingleOrDefault待定
         }
 
         public int Count()
         {
-            throw new NotImplementedException();
+            return Set.Count();
         }
 
         public long LongCount()
         {
-            throw new NotImplementedException();
+            return Set.LongCount();
         }
 
         public int Count(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Set.Count(predicate);
         }
 
         public long LongCount(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Set.LongCount(predicate);
         }
 
         public bool IsExists(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Set.Any(predicate);
         }
 
         public List<T> Search()
         {
-            throw new NotImplementedException();
+            return Set.ToList();
         }
 
         public List<T> Search(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Set.Where(predicate).ToList();
         }
 
         public IEnumerable<T> Query()
         {
-            throw new NotImplementedException();
+            return Set;
         }
 
         public IEnumerable<T> Query(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Set.Where(predicate);
         }
 
         public List<T> Search<P>(Expression<Func<T, bool>> predicate, Expression<Func<T, P>> order)
         {
-            throw new NotImplementedException();
+            return Search(predicate, order, false);
         }
 
         public List<T> Search<P>(Expression<Func<T, bool>> predicate, Expression<Func<T, P>> order, bool isDesc)
         {
-            throw new NotImplementedException();
+            var result = Set.Where(predicate);
+            if (isDesc)
+            {
+                result = result.OrderByDescending(order);
+            }
+            else
+            {
+                result =  result.OrderBy(order);
+            }
+            return result.ToList();
         }
 
         public PageModel<T> Search(PageCondition<T> condition)
