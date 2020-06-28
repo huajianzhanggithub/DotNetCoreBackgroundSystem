b64a3341c10d17de3a58984f86c8d4454ea963ca
huajian zhang <278554189@qq.com>
2020-06-28 12:49:18 +0800

    2020年6月28日 12点49分

--- Service.Implements/SysUserServiceImplement.cs
+++ Service.Implements/SysUserServiceImplement.cs
@@ -4,26 +4,41 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Domain.Repository;
+using Domain.Implements.Infrastructure;
+using Utils;
+using Domain.Implements.Repository;
 
 namespace Service.Implements
 {
     public class SysUserServiceImplement : BaseServiceImplement<SysUserInfoEntity>, ISysUserService
     {
-        protected ISysUserAuthSearchRepository AuthRepository { get; }
-        protected ISysUserInfoSearchRepository InfoRepository { get; }
-        public SysUserServiceImplement(ISysUserAuthSearchRepository authRepository, ISysUserInfoSearchRepository infoRepository)
+        protected SysUserAuthRepository AuthRepository { get; }
+        protected SysUserInfoRepository InfoRepository { get; }
+        public SysUserServiceImplement(SysUserAuthRepository authRepository, SysUserInfoRepository infoRepository):base(infoRepository)
         {
             AuthRepository = authRepository;
             InfoRepository = infoRepository;
         }
         public void ChangePassword(int userId, string oldPwd, string newPwd)
         {
-            throw new NotImplementedException();
+            var info = InfoRepository.Get(userId);
+            var auth = AuthRepository.Get(info.SysUserAuthId);
+            if (oldPwd==null || oldPwd!=auth?.Password)
+            {
+                throw new Exception("原密码错误！");
+            }
+            auth.Password = newPwd;
         }
 
         public void Register(SysUserAuthEntity auth, SysUserInfoEntity info)
         {
-            throw new NotImplementedException();
+            var authItem = AuthRepository.Get(p => p.LoginType == auth.LoginType && p.UserName == auth.UserName);
+            if (authItem!=null)
+            {
+                throw new Exception("用户信息已经存在！");
+            }
+            info.SysUserAuth = auth;
+            InfoRepository.Insert(info);
         }
     }
 }
