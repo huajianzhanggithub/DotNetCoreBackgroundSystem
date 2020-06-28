adad7dc0fc38c969cd12ac2c575eb75ee2d51b4b
huajian zhang <278554189@qq.com>
2020-06-28 18:03:03 +0800

    2020年6月28日 18点03分

---
+++ Utils/Security/MD5Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Utils.Security
+{
+    public static class MD5Helper
+    {
+        private static MD5 Handler { get; } = new MD5CryptoServiceProvider();
+        public static string GetMD5Str(string source)
+        {
+            var data = Encoding.UTF8.GetBytes(source);
+            var security = Handler.ComputeHash(data);
+            var sb = new StringBuilder();
+            foreach (var item in security)
+            {
+                sb.Append(item.ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
--- Utils/Security/RSAHelper.cs
+++ Utils/Security/RSAHelper.cs
@@ -7,14 +7,64 @@ namespace Utils.Security
 {
     public static class RSAHelper
     {
-        public static RSAParameters publicKey { get; private set; }
-        public static RSAParameters privateKey { get; private set; }
+        public static RSAParameters PublicKey { get; private set; }
+        public static RSAParameters PrivateKey { get; private set; }
         public static void InitWindows()
         {
             var parameters = new CspParameters()
-            { 
+            {
                 KeyContainerName = "RSAHELPER" //默认的RSA保存密钥的容器名称}
             };
+            var handle = new RSACryptoServiceProvider(parameters);
+            PublicKey = handle.ExportParameters(false);
+            PrivateKey = handle.ExportParameters(true);
+        }
 
+        public static void ExportKeyPair(string publicKeyXmlString, string privateKeyXmlString)
+        {
+            var handle = new RSACryptoServiceProvider();
+            handle.FromXmlString(publicKeyXmlString);
+            PublicKey = handle.ExportParameters(false);
+            handle.FromXmlString(privateKeyXmlString);
+            PrivateKey = handle.ExportParameters(true);
+        }
+
+        public static byte[] Encrypt(byte[] dataToEncrypt)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(PublicKey);
+                    encryptedData = RSA.Encrypt(dataToEncrypt, true);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public static byte[] Decrypt(byte[] dataToDecrypt)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(PrivateKey);
+                    decryptedData = rsa.Decrypt(dataToDecrypt, true);
+                    return decryptedData;
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
     }
 }
